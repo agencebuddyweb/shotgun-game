075f3346bd56eadac869c205f333c3defc51cbc7
Sébastien <Conejo>
2023-02-15 20:38:55 +0100

    feat: add home and 404 pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './pages/errors/error404/error404.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  // Pages.
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '404',
+    component: Error404Component,
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './pages/errors/error404/error404.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, Error404Component],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
-export class AppModule { }
+export class AppModule {}
