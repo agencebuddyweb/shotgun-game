13b2cc87d1187b0bb704e9c4ebc72c9a0b0ef3ef
Sébastien <Conejo>
2023-02-16 18:04:36 +0100

    feat: add bot create page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BotCreateComponent } from './pages/bot-create/bot-create.component';
 import { Error404Component } from './pages/errors/error404/error404.component';
 import { HomeComponent } from './pages/home/home.component';
 
@@ -8,6 +9,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'create-bot',
+    component: BotCreateComponent,
   },
   {
     path: '404',
