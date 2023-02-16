0a82c7e11657fcb5dff8037724a3e6208cc43e5d
Sébastien <Conejo>
2023-02-16 12:22:08 +0100

    feat: add header and default style components and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { Error404Component } from './pages/errors/error404/error404.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, Error404Component],
+  declarations: [AppComponent, HomeComponent, Error404Component, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
