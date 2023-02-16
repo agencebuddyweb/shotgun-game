737109ae6bfb8f3a9efe94f847e78a838d32eaaa
Sébastien <Conejo>
2023-02-16 15:12:43 +0100

    feat: add card with custom style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { Error404Component } from './pages/errors/error404/error404.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
+import { RankItemComponent } from './partials/rank-item/rank-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, Error404Component, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HomeComponent, Error404Component, HeaderComponent, FooterComponent, RankItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/partials/rank-item/rank-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rank-item',
+  templateUrl: './rank-item.component.html',
+  styleUrls: ['./rank-item.component.scss']
+})
+export class RankItemComponent {
+
+}
