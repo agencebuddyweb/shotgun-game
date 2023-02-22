556981921e5919b0b0e48922c89f31e199d6eaae
Ismaël Guerrib <ismael@buddyweb.fr>
2023-02-22 14:07:26 +0100

    fix(bot-avatar): name dont display

--- ui-refine/src/pages/bots/avatar.tsx
+++ ui-refine/src/pages/bots/avatar.tsx
@@ -2,13 +2,13 @@ import { IBot } from "interfaces";
 
 function BotAvatar(props:{bot:IBot,isNameAtLeft:boolean}) {
   return (
-    <div>
-      <span className="is-size-5 ml-3 has-text-weight-bold">{props.isNameAtLeft && props.bot.name}</span>
+    <>
+      <span className="is-size-5 ml-3 has-text-weight-bold">{!props.isNameAtLeft && props.bot.name}</span>
       <div className="avatar ml-2">
-        <img className="avatar-img" src={props.bot.avatar} alt={props.bot.name} />
-        <span className="is-size-5 ml-3 has-text-weight-bold">{!props.isNameAtLeft && props.bot.name}</span>
+      <img className="avatar-img" src={props.bot.avatar} alt={props.bot.name} />
       </div>
-    </div>
+      <span className="is-size-5 ml-3 has-text-weight-bold">{props.isNameAtLeft && props.bot.name}</span>
+    </>
   );
 }
 export default BotAvatar;
